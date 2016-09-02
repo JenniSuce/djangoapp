80172c8e22ec8f5c619ed0f2d4c0efb2b833f90c
JenniSu <jenni_su@hotmail.com>
2016-09-02 09:17:01 -0600

    Mi primer commit

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+
+class Postear(models.Model):
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    creacion_date = models.DateTimeField(
+                    default=timezone.now)
+    publicacion_date = models.DateTimeField(
+                        blank=True, null=True)
+
+    def publicar(self):
+        self.publicacion_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
