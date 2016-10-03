a3b13b024671af4d22987264bbd277e8b3c5edfd
JenniSu <jenni_su@hotmail.com>
2016-10-02 23:51:04 -0600

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Postear
+from django.utils import timezone
+
 # Create your views here.
 def listar_articulo(request):
     articulo = Postear.objects.all()
     return render(request, 'blog/listar_articulo.html', {'articulo':articulo})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Postear, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
