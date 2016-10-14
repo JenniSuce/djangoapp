92aca6baa634c2c487a883fbd896785fbd059a77
JenniSu <jenni_su@hotmail.com>
2016-10-14 09:39:41 -0600

    Added nuevos y editar blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.listar_articulo),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.postear_nuevo, name='postear_nuevo'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.editar_articulo, name='editar_articulo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Postear
 from django.utils import timezone
+from .forms import PostearForm
 
 # Create your views here.
 def listar_articulo(request):
@@ -10,3 +11,31 @@ def listar_articulo(request):
 def post_detail(request, pk):
     post = get_object_or_404(Postear, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def postear_nuevo(request):
+    if request.method == "POST":
+        formulario = PostearForm(request.POST)
+        if formulario.is_valid():
+            postear = formulario.save(commit=False)
+            postear.autor = request.user
+            postear.publicacion_date = timezone.now()
+            postear.save()
+            return redirect('blog.views.post_detail', pk=postear.pk)
+    else:
+        formulario = PostearForm()
+    formulario = PostearForm()
+    return render(request, 'blog/editar_articulo.html', {'formulario': formulario})
+
+def editar_articulo(request, pk):
+    articulo = get_object_or_404(Postear, pk=pk)
+    if request.method == "POST":
+        formulario = PostearForm(request.POST, instance=articulo)
+        if formulario.is_valid():
+            articulo = formulario.save(commit=False)
+            articulo.autor = request.user
+            articulo.save()
+            return redirect('blog.views.post_detail', pk=articulo.pk)
+    else:
+        formulario = PostearForm(instance=articulo)
+    #formulario = PostearForm()
+    return render(request, 'blog/editar_articulo.html', {'formulario': formulario})
